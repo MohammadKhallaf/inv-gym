28509d5295e1dde04a6a6056579cde824e5d908b
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-11-10 19:23:26 +0200

    some update s

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Layout from "./layout/index";
-import "./App.css";
+import "./styles/app.scss";
 
 function App() {
   return <Layout>test</Layout>;
